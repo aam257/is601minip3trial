d8999d784a8cb764b46a70cd777dfb95841a1a97
aniket mirajkar <aam257@njit.edu>
2018-11-21 01:01:30 -0500

    Task: Added answer migrate, model and seeder. Answer tests also added.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserTableSeeder::class);
         $this->call(QuestionTableSeeder::class);
+        $this->call(AnswerTableSeeder::class);
+
     }
 }
